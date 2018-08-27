25f5ccb661692787470f7f437b893b1298069d64
Connor Stewart <Connor@DESKTOP-GN328TV.home>
2018-08-27 17:21:26 +0100

    Fully working A*!

--- DungeonCrawler/Tile.cpp
+++ DungeonCrawler/Tile.cpp
@@ -1,8 +1,14 @@
 #include "stdafx.h"
 #include "Tile.h"
 
-Tile::Tile(std::pair<int, int> location, int index) : sf::RectangleShape(), Node(location, index) {}
+Tile::Tile(std::pair<int, int> gridLoc, std::pair<float, float> worldLoc, bool filled, sf::Vector2f size) : sf::RectangleShape(size), Node(gridLoc, filled) {
+	this->worldLoc = worldLoc;
+}
+
+std::pair<float, float> Tile::getWorldLoc(){
+	return worldLoc;
+}
 
 bool operator==(const Tile& lhs, const Tile& rhs) {
-	return lhs.location == rhs.location;
+	return lhs.gridLocation == rhs.gridLocation;
 }
